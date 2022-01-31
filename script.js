9068361383246779b7c1e53a2e4a937a01935ad1
tsmolarczyk <smolarczyk.thomas@gmail.com>
2022-01-31 23:15:58 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const movies = [
+  {
+    id: 1,
+    title: "Titanic",
+    thumb:
+      "https://cdn.cinematerial.com/p/297x/m5ieu5ug/titanic-ii-dvd-movie-cover-md.jpg",
+  },
+  {
+    id: 2,
+    title: "Szklana Pułapka",
+    thumb: "https://fwcdn.pl/fpo/12/70/1270/7382745.3.jpg",
+  },
+];
+
+const titleElement = document.querySelector(".title-input");
+const movieList = document.querySelector(".movie-list");
+
+// https://www.themoviedb.org/documentation/api
+// 057eb33b4af7c84b07e838484bbf482c
+
+function render() {
+  movies.forEach((movie) => {
+    // empty boxes
+    const movieElement = document.createElement("div");
+    const movieIdElement = document.createElement("h1");
+    const movieTitleElement = document.createElement("div");
+    const movieThumbElement = document.createElement("img");
+
+    // taking value
+    movieIdElement.textContent = movie.id;
+    movieTitleElement.textContent = movie.title;
+    movieThumbElement.src = movie.thumb;
+
+    // adding classes
+    movieTitleElement.classList.add("movie-title");
+
+    // adding elements
+    movieList.appendChild(movieElement);
+    movieElement.appendChild(movieIdElement);
+    movieElement.appendChild(movieTitleElement);
+    movieElement.appendChild(movieThumbElement);
+  });
+}
+
+render();
