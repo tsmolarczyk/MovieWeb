849f8945c1dbc33e225365b174deeb1cfe14a854
tsmolarczyk <smolarczyk.thomas@gmail.com>
2022-02-16 19:42:50 +0100

    error

--- src/components/movie-list/movieList.js
+++ src/components/movie-list/movieList.js
@@ -3,22 +3,32 @@ const API_KEY = "57b4025ea3b2beb4d12b65e71d4dc270";
 const titleElement = document.querySelector(".title-input");
 const movieList = document.querySelector(".movie-list");
 const searchInputElement = document.querySelector(".nav__search--input");
+const getPopularBtn = document.querySelector(".get-popular-btn");
 const searchBtn = document.querySelector(".search-btn");
 const navBar = document.querySelector(".nav");
-
 const moreMovieBtn = document.createElement("button");
-
 const modalDescription = document.querySelector(".modal-description");
+const previousPageBtn = document.createElement("button");
+const nextPageBtn = document.createElement("button");
 
-// assets/
-// - images
-// - fonts
-// src
-// - components
-//  -- movie-list
-//   --- movie-list.js
-//   --- movie-list.css
-//
+const footer = document.querySelector(".footer");
+
+let options = {
+  // root: document.querySelector("body"),
+  rootMargin: "0px",
+  threshold: 0.5,
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      console.log(entry);
+      handleNext();
+      observer.unobserve(entry.target);
+    }
+  });
+}, options);
 
 let state = {
   movies: [],
@@ -29,56 +39,75 @@ let state = {
   onePageLoading: 0,
 };
 
-////////////////////////////////////////////////////////////////
-// dynamic searching before debouncing
+previousPageBtn.textContent = "previous";
+nextPageBtn.textContent = "next";
 
-// searchInputElement.addEventListener("keyup", () => {
-//   searchDuringWrite();
-// });
+nextPageBtn.classList.add("next-page-btn");
+previousPageBtn.classList.add("previous-page-btn");
 
-// prettier-ignore
+navBar.appendChild(nextPageBtn);
+navBar.appendChild(previousPageBtn);
 
-const debounce = (fn, delay) => {     // pobiera 2 argumenty, 1)funcja 2)delay
-    let timeoutId;                      // inicjalizacja zmiennej, zeby mozna bylo resetowac 
-    return function (...args) {         // zwraca funcje ze wszystkich podanych argumentow -> ...args -> rest parameter bierze ostatni parametr i dodaje do wczesniejszego
-      if (timeoutId) {
-        clearTimeout(timeoutId);        // jesli jest timeoutId to zeruj go, zaczynaj od poczatku odliczanie, 
-      }
-      timeoutId = setTimeout(() => {    // wywolanie funckcji z opoznieniem, ustawienie tez timeouta 
-        fn(...args);
-      }, delay);
-    };
-  };
+nextPageBtn.addEventListener("click", handleNext);
+
+getPopularBtn.addEventListener("click", fetchPopularMovies);
+
+// moreMovieBtn.addEventListener("click", loadMoreMoviesByBtn);
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+
+previousPageBtn.addEventListener("click", () => {
+  if (state.page <= 1) {
+    return;
+  }
+  state.page--;
+  fetchByQuery();
+});
 
 searchInputElement.addEventListener(
   "keyup",
   debounce(() => {
-    searchDuringWrite();
+    state.onePageLoading = 1;
+    fetchByQuery();
   }, 1000)
 );
 
-function searchDuringWrite() {
-  const searchInput = searchInputElement.value;
-  if (searchInput.length >= 3) {
-    fetchByQuery();
-  }
+function handleNext() {
+  state.page++;
+  fetchByQuery();
+}
+
+function debounce(fn, delay) {
+  // pobiera 2 argumenty, 1)funcja 2)delay
+  let timeoutId; // inicjalizacja zmiennej, zeby mozna bylo resetowac
+  return function (...args) {
+    // zwraca funcje ze wszystkich podanych argumentow -> ...args -> rest parameter bierze ostatni parametr i dodaje do wczesniejszego
+    if (timeoutId) {
+      clearTimeout(timeoutId); // jesli jest timeoutId to zeruj go, zaczynaj od poczatku odliczanie,
+    }
+    timeoutId = setTimeout(() => {
+      // wywolanie funckcji z opoznieniem, ustawienie tez timeouta
+      fn(...args);
+    }, delay);
+  };
 }
 
 ////////////////////////////////////////////////////////////////
 
 function fetchPopularMovies() {
+  state.onePageLoading = 0;
   // zerowanie inputa, zeby popular nie gryzl sie z searchem potem
   searchInputElement.value = "";
   getMovies(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
 }
 
 function fetchByQuery() {
-  observer.observe(moreMovieBtn);
   const searchInput = searchInputElement.value;
-  getMovies(
-    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchInput}&page=${state.page}`
-  );
-  observer.unobserve(moreMovieBtn);
+  if (searchInput.length >= 3) {
+    getMovies(
+      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchInput}&page=${state.page}`
+    );
+  }
 }
 
 function fetchDetails(id) {
@@ -92,53 +121,30 @@ function fetchDetails(id) {
   );
 }
 
-// function fetchDetails(id) {
-//   fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       state.movieDetails = data;
-//       openModal();
-//     });
-// }
-
 function getMovies(link) {
   fetch(link)
     .then((res) => res.json())
     .then((data) => {
       let movies = data.results;
-      console.log(movies);
       state.movies = movies;
       state.ready = true;
       render();
     });
 }
 
-const previousPageBtn = document.createElement("button");
-const nextPageBtn = document.createElement("button");
+function handleMovieClick(id) {
+  fetchDetails(id);
+}
 
-previousPageBtn.textContent = "previous";
-nextPageBtn.textContent = "next";
+// LOADING MORE MOVIES BY BUTTON
 
-nextPageBtn.classList.add("next-page-btn");
-previousPageBtn.classList.add("previous-page-btn");
-
-navBar.appendChild(nextPageBtn);
-navBar.appendChild(previousPageBtn);
-
-nextPageBtn.addEventListener("click", () => {
-  state.page++;
-  fetchByQuery();
-});
-
-// https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
-
-previousPageBtn.addEventListener("click", () => {
-  if (state.page <= 1) {
-    return;
-  }
-  state.page--;
-  fetchByQuery();
-});
+// function loadMoreMoviesByBtn() {
+//   state.onePageLoading = 1;
+//   console.log(state.onePageLoading);
+//   state.page++;
+//   fetchByQuery();
+//   console.log(state.page);
+// }
 
 function render() {
   if (state.onePageLoading === 0) {
@@ -153,7 +159,7 @@ function render() {
     return;
   }
 
-  state.movies.forEach((movie) => {
+  state.movies.forEach((movie, index) => {
     // empty boxes
     const movieElement = document.createElement("div");
     const movieVoteElement = document.createElement("h1");
@@ -180,46 +186,10 @@ function render() {
     movieElement.appendChild(movieThumbElement);
 
     movieElement.addEventListener("click", () => handleMovieClick(movie.id));
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        console.log("observ!!!");
-        loadMoreMoviesByBtn();
-      });
-    }, options);
+
+    movieList.appendChild(moreMovieBtn);
   });
-
-  movieList.appendChild(moreMovieBtn);
-  state.onePageLoading = 0;
+  if (state.onePageLoading === 1) {
+    observer.observe(footer);
+  }
 }
-
-function handleMovieClick(id) {
-  fetchDetails(id);
-}
-
-// LOADING MORE MOVIES BY BUTTON
-
-moreMovieBtn.addEventListener("click", loadMoreMoviesByBtn);
-
-function loadMoreMoviesByBtn() {
-  state.onePageLoading = 1;
-  console.log(state.onePageLoading);
-  state.page++;
-  fetchByQuery();
-  console.log(state.page);
-}
-
-// LOADING MORE MOVIES BY INTERSECTION-OBSERVER
-const footer = document.querySelector(".footer");
-
-let options = {
-  root: movieList,
-  rootMargin: "0px",
-  threshold: 1,
-};
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log("observ!!!");
-    loadMoreMoviesByBtn();
-  });
-}, options);
